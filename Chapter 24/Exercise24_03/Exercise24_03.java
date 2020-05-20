3d01e0893d1621a8818d0c4c8856af90b8bfcb53
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2020-05-20 09:56:13 -0600

    Add files via upload

--- Chapter 24/Exercise24_03/Exercise24_03.java
+++ Chapter 24/Exercise24_03/Exercise24_03.java
@@ -1,3 +1,26 @@
+/*
+Assignment Author: Jason Waters
+Date: 5/20/2020
+Exercise: 24-03
+Class: CSCI 1115
+Discription: 
+(Implement a doubly linked list) The MyLinkedList class used in Listing 24.6 is a one-way directional linked list that enables one-way traversal of the list. A doubly linked list enables two-way traversal. To do this, the Node class needs to be modified to add the new data field name previous to refer to the previous node in the list, as follows:
+
+public class Node<E> { 
+	E element;
+	Node<E> next; 
+	Node<E> previous; 
+
+	public Node(E e) { 
+		element = e; 
+	}
+} 
+Implement a new class named TwoWayLinkedList that uses a doubly linked list to store elements. We have built a majority of the Exercise24_3 class for you, including Node and the iterators. Make edits to the TwoWayLinkedList class where told in the comments (lines 47, 65, 87, and 88).
+
+
+*/
+
+
 class Exercise24_03 {
 	public static void main(String[] args) {
 		TwoWayLinkedList<Integer> list = new TwoWayLinkedList<Integer>();
