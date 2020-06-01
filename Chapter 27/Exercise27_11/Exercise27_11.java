6aef3178c19662ad751d14348bd59d6364e49a51
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2020-06-01 14:45:11 -0600

    Add files via upload

---
+++ Chapter 27/Exercise27_11/Exercise27_11.java
@@ -0,0 +1,43 @@
+/*
+Assignment Author: Jason Waters
+Date: 6/1/2020
+Exercise: 27-11
+Class: CSCI 1115
+Discription: 
+
+(setToList) Write the following method that returns an ArrayList from a set. HerePreview the document is the test class. 
+
+public static <E> ArrayList<E> setToList(Set<E> s)
+Remember that a set will not contain duplicates.
+*/
+
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+
+public class Exercise27_11 {
+  public static void main(String[] args) {
+    Set<String> set = new HashSet<String>(); 
+    set.add("Smith");
+    set.add("Anderson");
+    set.add("Lewis");
+    set.add("Cook"); 
+    set.add("Smith");
+    
+    ArrayList<String> list = setToList(set);
+    System.out.println(list);
+  }
+  
+  public static <E> ArrayList<E> setToList(Set<E> s) {
+    /*
+    Insert your code here
+    */
+    ArrayList<E> list = new ArrayList<>();
+    
+    for (E e: s) {
+        list.add(e);
+    }
+    return list;
+  }
+}
