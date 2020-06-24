d35e881df77b2ebe4b1cb35451a767a50274ef3b
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2020-06-24 12:20:09 -0600

    Add files via upload

---
+++ Chapter 31/Exercise31_01/Exercise31_01Client.java
@@ -0,0 +1,113 @@
+// Exercise31_01Client.java: The client sends the input to the server and receives
+// result back from the server
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;import java.io.*;
+import java.net.*;
+
+
+public class Exercise31_01Client extends Application {
+  //IO streams
+  DataOutputStream toServer = null;
+  DataInputStream fromServer = null;
+  
+  // Text field for receiving radius
+  private TextField tfAnnualInterestRate = new TextField();
+  private TextField tfNumOfYears = new TextField();
+  private TextField tfLoanAmount = new TextField();
+  private Button btSubmit= new Button("Submit");
+
+  // Text area to display contents
+  private TextArea ta = new TextArea();
+  
+  @Override // Override the start method in the Application class
+  public void start(Stage primaryStage) {
+    ta.setWrapText(true);
+   
+    GridPane gridPane = new GridPane();
+    gridPane.add(new Label("Annual Interest Rate"), 0, 0);
+    gridPane.add(new Label("Number Of Years"), 0, 1);
+    gridPane.add(new Label("Loan Amount"), 0, 2);
+    gridPane.add(tfAnnualInterestRate, 1, 0);
+    gridPane.add(tfNumOfYears, 1, 1);
+    gridPane.add(tfLoanAmount, 1, 2);
+    gridPane.add(btSubmit, 2, 1);
+    
+    tfAnnualInterestRate.setAlignment(Pos.BASELINE_RIGHT);
+    tfNumOfYears.setAlignment(Pos.BASELINE_RIGHT);
+    tfLoanAmount.setAlignment(Pos.BASELINE_RIGHT);
+    
+    tfLoanAmount.setPrefColumnCount(5);
+    tfNumOfYears.setPrefColumnCount(5);
+    tfLoanAmount.setPrefColumnCount(5);
+            
+    BorderPane pane = new BorderPane();
+    pane.setCenter(new ScrollPane(ta));
+    pane.setTop(gridPane);
+    
+    // Create a scene and place it in the stage
+    Scene scene = new Scene(pane, 400, 250);
+    primaryStage.setTitle("Exercise31_01Client"); // Set the stage title
+    primaryStage.setScene(scene); // Place the scene in the stage
+    primaryStage.show(); // Display the stage
+    
+    btSubmit.setOnAction(e -> {
+      try{
+        //get annual interest rate from tf
+        double annualInterestRate = Double.parseDouble(tfAnnualInterestRate.getText().trim());
+        int numberOfYears = Integer.parseInt(tfNumOfYears.getText().trim());
+        double loanAmount = Double.parseDouble(tfLoanAmount.getText().trim());
+        
+        //send to server
+        toServer.writeDouble(annualInterestRate);
+        toServer.writeInt(numberOfYears);
+        toServer.writeDouble(loanAmount);
+        toServer.flush();
+
+        //get data from server
+        Double monthlyPayment = fromServer.readDouble();
+        Double totalPayment = fromServer.readDouble();
+        
+        //display text
+        ta.appendText("Annual Interest Rate: " + annualInterestRate + '\n');
+        ta.appendText("Number of Years: " + numberOfYears + '\n');
+        ta.appendText("Loan Amount: " + loanAmount + '\n');
+        ta.appendText("Monthly Payment: " + monthlyPayment + '\n');
+        ta.appendText("Total Payment: " + totalPayment + '\n');
+        
+        }
+        catch(IOException ex){
+          System.err.println(ex);
+        }
+    });
+    try {
+      //create socket to connect to server
+      Socket socket = new Socket("localhost", 8000);
+      
+      //Input Stream from server
+      fromServer = new DataInputStream(socket.getInputStream());
+      
+      //send to server socket
+      toServer = new DataOutputStream(socket.getOutputStream());
+      
+    } catch (IOException ex) {
+            ta.appendText(ex.toString() + '\n');
+    }
+  }
+  
+  /**
+   * The main method is only needed for the IDE with limited
+   * JavaFX support. Not needed for running from the command line.
+   */
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
---
+++ Chapter 31/Exercise31_01/Exercise31_01Server.java
@@ -0,0 +1,97 @@
+/*
+Assignment Author: Jason Waters
+Date: 6/23/2020
+Exercise: 31-01
+Class: CSCI 1115
+Discription: 
+(Loan server) Write a server for a client. The client sends loan information (annual interest rate, number of years, and loan amount) to the server. The server creates a Loan object to compute monthly payment and total payment, and sends those numbers back to the client.
+*/
+
+// Exercise31_01Server.java: The server can communicate with
+// multiple clients concurrently using the multiple threads
+import java.util.*;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;import java.net.*;
+import javax.xml.crypto.*;
+import java.io.*;
+import java.sql.*;
+import java.util.Date;
+
+
+public class Exercise31_01Server extends Application {
+  // Text area for displaying contents
+  private TextArea ta = new TextArea();
+
+  @Override // Override the start method in the Application class
+  public void start(Stage primaryStage) {
+    ta.setWrapText(true);
+   
+    // Create a scene and place it in the stage
+    Scene scene = new Scene(new ScrollPane(ta), 400, 200);
+    primaryStage.setTitle("Exercise31_01Server"); // Set the stage title
+    primaryStage.setScene(scene); // Place the scene in the stage
+    primaryStage.show(); // Display the stage
+    
+    new Thread(() -> {
+        try{
+          //create server socket
+          ServerSocket serverSocket = new ServerSocket(8000);
+          Platform.runLater(() -> 
+            ta.appendText("Exercise31_01Server started at " + new Date() + '\n'));
+            
+          //listen for connection request
+          Socket socket = serverSocket.accept();
+          
+          //create data streams
+          DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
+          DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
+          
+          
+          
+          while (true){
+            Date dateClientConnected = new Date();
+            //recieve information from client
+            double annualIntrestRate = inputFromClient.readDouble();
+            int numberOfYears = inputFromClient.readInt();
+            double loanAmount = inputFromClient.readDouble();
+            
+            Loan loan = new Loan(annualIntrestRate, numberOfYears, loanAmount);
+            
+            //compute payments
+            double monthlyIntrestRate = loan.getAnnualInterestRate();
+            double monthlyPayment = loan.getMonthlyPayment();
+            double totalPayment = loan.getTotalPayment();
+            
+            //send payment data back to client
+            outputToClient.writeDouble(monthlyPayment);
+            outputToClient.writeDouble(totalPayment);
+          
+            
+            Platform.runLater(() -> {
+              ta.appendText("Connected to a client at " + dateClientConnected + '\n');
+              ta.appendText("Annual Interest Rate: " + annualIntrestRate + '\n');
+              ta.appendText("Number of Years: " + numberOfYears + '\n');
+              ta.appendText("Loan Amount: " + loanAmount + '\n');
+              ta.appendText("Monthly Payment: " + monthlyPayment + '\n');
+              ta.appendText("Total Payment: " + totalPayment + '\n');
+            });
+          }  
+        }
+        catch (IOException ex) {
+          ex.printStackTrace();
+        }
+    }).start();
+  }
+    
+  /**
+   * The main method is only needed for the IDE with limited
+   * JavaFX support. Not needed for running from the command line.
+   */
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
