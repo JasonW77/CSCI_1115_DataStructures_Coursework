9e1f408bcb78d57d8f84deb4b6c09b29cca0e7d0
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2020-07-07 19:12:44 -0600

    Add files via upload

--- FinalProject/Pantry.java
+++ FinalProject/Pantry.java
@@ -1,60 +1,82 @@
-public class Pantry {
-	private String itemName;
-	private int quan;
-	private int row;
-	private int column;
-	private java.util.Date exp;
-	private java.util.Date entry;
-	
-	/** Default constructor */
-	public Pantry(){
+package FinalProject;
 
+import java.util.Date;
+import java.text.*;
+import java.io.*;
+import java.util.*;
+
+public class Pantry{
+	SimpleDateFormat eDate = new SimpleDateFormat("dd/mm/yy");
+	private String iN;
+	private int iQ;
+	private int iRL;
+	private int iCL;
+	private java.util.Date iE;
+	private java.util.Date entryDate;
+	
+	//Default constructor 
+	public Pantry(){
+		this.iN = "Null";
+		this.iQ = 0;
+		try {
+			this.iE = eDate.parse("01/01/01");
+		} catch (ParseException parseEx) {
+			parseEx.printStackTrace();
+		}
+		
+		this.iRL = 0;
+		this.iCL = 0;
+
+		entryDate = new java.util.Date();
 	}
 	
-	/** Construct a Pantry Object with specified itemName, quan, row, column, exp, entry */
-	public Pantry(String itemName, int quan, int row, int column){
-		this.itemName = itemName;
-		this.quan = quan;
-		this.row = row;
-		this.column = column;
-		this.exp = exp;
-		entry = new java.util.Date();
+	/** Construct a Pantry Object with specified iN, iQ, iRL, iCL, iE, entryDate */
+	public Pantry(String iN, int iQ, java.util.Date iE, int iRL, int iCL){
+		this.iN = iN;
+		this.iQ = iQ;
+		this.iE = iE;
+		this.iRL = iRL;
+		this.iCL = iCL;
+		entryDate = new java.util.Date();
 	}
 	
 	/** Getters */
-	public String getItemName(){
-		return itemName;
+	public String getiN(){
+		return iN;
 	}
 	public int getQuantity(){
-		return quan;
+		return iQ;
 	}
 	public int getRow(){
-		return row;
+		return iRL;
 	}
 	public int getColumn(){
-		return column;
+		return iCL;
 	}
 	public java.util.Date getExpiration(){
-		return exp;
+		return iE;
 	}
 	public java.util.Date getEnterDate(){
-		return entry;
+		return entryDate;
 	}
 	/** Setters */
-	public void setItemName(String itemName){
-		this.itemName = itemName;
+	public void setItemName(String iN){
+		this.iN = iN;
 	}
-	public void setQuantity(int quan){
-		this.quan = quan;
+	public void setQuantity(int iQ){
+		this.iQ = iQ;
 	}
-	public void setLocation(int row, int column){
-		this.row = row;
-		this.column = column;
+	public void setLocation(int iRL, int iCL){
+		this.iRL = iRL;
+		this.iCL = iCL;
 	}
-	public void setExpiration(java.util.Date exp){
-		this.exp = exp;
+	public void setExpiration(java.util.Date iE){
+		this.iE = iE;
 	}
-	public void setEnterDate(java.util.Date entry){
-		this.entry = entry;
+	public void setEnterDate(java.util.Date entryDate){
+		this.entryDate = entryDate;
+	}
+	public String toString(){
+		return "Pantry: \n" + " Item Name: " + iN + "\n Item Quantitiy: " + iQ + "\n Item Row Location: " + iRL + "\n Item Column Location: " + iCL + "\n Item Expiration Date: " + iE + "\n Item Entry Date: " + entryDate;
 	}
 }
--- FinalProject/Pantry_Goo_ee.java
+++ FinalProject/Pantry_Goo_ee.java
@@ -1,3 +1,16 @@
+/**
+ * <h1>JavaDoc</h1>
+ * 
+ *  This is my Pantry application interface class (or Pantry_Goo_ee.java).
+ * 	I created this program for the CSCI_1115_DataStructures_Final_Project
+ * 	 
+ * 
+	<p>Date created 7/7/2020 <p/>
+	
+	@author Jason Waters
+ */
+package FinalProject;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -8,20 +21,47 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.geometry.Insets;
 import javafx.scene.input.KeyCode;
+import java.net.*;
+import java.text.*;
+import java.util.*;
+import java.io.*;
+import java.io.Serializable;
 
-//import java.awt.*;
+
 
 public class Pantry_Goo_ee extends Application {
+	
 	@Override //Override the start method in the Application class
 	public void start(Stage primaryStage){
-		//Create a scene
-		Button btOk = new Button("Enter");
-		Button btSea = new Button("Search");
+		
+		Map<String,Object> map = new HashMap<>();
+		/*java.io.File file = new java.io.File("pantry.txt");
+		
+
+		try (
+			//read from file "pantry.dat"
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
+			){
+				map.put(ois.readObject());
+				System.out.println("Object read from file");
+				
+			}catch(Exception e){
+				e.printStackTrace();
+
+				System.out.println("File not found.");
+			}
+		*/
+
+		Button btFinish = new Button("Finish");
+		Button btNewItem = new Button("Enter New Item");
+		Button btItemSearch = new Button("Item Search");
+		Button btClearEntry = new Button("Clear Entry");
 		
 		TextField itemName = new TextField();
 		TextField itemQuan = new TextField();
 		TextField itemExp = new TextField();
-		TextField itemLoc = new TextField();
+		TextField itemRLoc = new TextField();
+		TextField itemCLoc = new TextField();
 		
 		VBox vb = new VBox();
 		
@@ -30,53 +70,155 @@ public class Pantry_Goo_ee extends Application {
 		Label label1 = new Label("Item Name: ");
 		Label label2 = new Label("Quantity: ");
 		Label label3 = new Label("Expiration: ");
-		Label label4 = new Label("Location: ");
+		Label label4 = new Label("Row Location: ");
+		Label label5 = new Label("Column Location: ");
 		
+		btFinish.setVisible(false);
+		btNewItem.setVisible(true);
+		btItemSearch.setVisible(true);
+		btClearEntry.setVisible(false);
+		
+		itemName.setVisible(false);
+		itemQuan.setVisible(false);
+		itemExp.setVisible(false);
+		itemRLoc.setVisible(false);
+		itemCLoc.setVisible(false);
+		
+		label1.setVisible(false);
+		label2.setVisible(false);
+		label3.setVisible(false);
+		label4.setVisible(false);
+		label5.setVisible(false);
+		
+
+		//Create a scene and a pane, then place them in the stage		
 		pane.setPadding(new Insets(10, 10, 10, 10));
 		pane.setVgap(5);
 		pane.setHgap(5);
 		
-		Scene scene = new Scene(pane, 200, 300);
+		Scene scene = new Scene(pane, 300, 350);
 		
 		pane.getChildren().addAll(vb);
-		vb.getChildren().addAll(label1, itemName, label2, itemQuan, label3, itemExp, label4, itemLoc,btOk,btSea);
+		vb.getChildren().addAll(label1, itemName, label2, itemQuan, label3, itemExp, label4, itemRLoc,label5, itemCLoc,btFinish,btNewItem,btItemSearch,btClearEntry);
 		
 		primaryStage.setTitle("Angie's Pantry");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
-		//set on actions
+		//set on actions for buttons
 		itemName.setOnKeyPressed(e -> {
 			if (e.getCode() == KeyCode.ENTER){
-				System.out.println("Remember to create itemName Method!");
+				if (itemQuan.isVisible() != true){
+				String seaItem;
+				seaItem = itemName.getText().trim();
+				System.out.println("Search Item Detected: " + seaItem);
+				System.out.println(map.get(seaItem));
+				}
 			}
 		});
-		itemQuan.setOnKeyPressed(e -> {
-			if (e.getCode() == KeyCode.ENTER){
-				System.out.println("Remember to create itemQuan Method!");
+		
+		btFinish.setOnAction(e -> {
+			//Enter new item constructor Constructor here
+			SimpleDateFormat eDate = new SimpleDateFormat("dd/mm/yy");
+			String iN = itemName.getText().trim();
+			int iQ = Integer.parseInt(itemQuan.getText().trim());
+			java.util.Date iE = new java.util.Date();
+			try {
+				iE = eDate.parse(itemExp.getText().trim());
+			} catch (ParseException parseEx) {
+				parseEx.printStackTrace();
+				
 			}
-		});
-		itemExp.setOnKeyPressed(e -> {
-			if (e.getCode() == KeyCode.ENTER){
-				System.out.println("Remember to create itemExp Method!");
+			
+			int iRL = Integer.parseInt(itemRLoc.getText().trim());
+			int iCL = Integer.parseInt(itemCLoc.getText().trim());
+			
+			Pantry pan = new Pantry(iN, iQ, iE, iRL, iCL);
+			map.put(pan.getiN(), pan);
+			/*
+			try (
+				ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("pantry.txt", true));
+				){
+				oos.writeObject(map);
+				System.out.println("Object written to file!");
+				
+			} catch (Exception ex) {
+				ex.printStackTrace();
 			}
-		});
-		itemLoc.setOnKeyPressed(e -> {
-			if (e.getCode() == KeyCode.ENTER){
-				System.out.println("Remember to create itemLoc Method!");
-			}
-		});
-		btOk.setOnAction(e -> {
-			
-				System.out.println("Remember to create btOk Method!");
+			*/	
+			System.out.println(pan.toString() + "\n");
+			System.out.println(map + "/n");
 			
 		});
-		btSea.setOnAction(e -> {
+		
+		btNewItem.setOnAction(e -> {
 			
-				System.out.println("Remember to create btSea Method!");
+			itemName.setVisible(true);
+			itemQuan.setVisible(true);
+			itemExp.setVisible(true);
+			itemRLoc.setVisible(true);
+			itemCLoc.setVisible(true);
 			
+			label1.setVisible(true);
+			label2.setVisible(true);
+			label3.setVisible(true);
+			label4.setVisible(true);
+			label5.setVisible(true);
+			
+			btFinish.setVisible(true);
+			btNewItem.setVisible(false);
+			btItemSearch.setVisible(false);
+			btClearEntry.setVisible(true);
+		
+		});
+		
+		btItemSearch.setOnAction(e -> {
+			btFinish.setVisible(false);
+			btNewItem.setVisible(false);
+			btItemSearch.setVisible(false);
+			btClearEntry.setVisible(true);
+			
+			itemName.setVisible(true);
+			itemQuan.setVisible(false);
+			itemExp.setVisible(false);
+			itemRLoc.setVisible(false);
+			itemCLoc.setVisible(false);
+			
+			label1.setVisible(true);
+			label2.setVisible(false);
+			label3.setVisible(false);
+			label4.setVisible(false);
+			label5.setVisible(false);
+			
+		});
+		
+		btClearEntry.setOnAction(e -> {
+			
+			btFinish.setVisible(false);
+			btNewItem.setVisible(true);
+			btItemSearch.setVisible(true);
+			btClearEntry.setVisible(false);
+			
+			itemName.setVisible(false);
+			itemName.setText(" ");
+			itemQuan.setVisible(false);
+			itemQuan.setText(" ");
+			itemExp.setVisible(false);
+			itemExp.setText(" ");
+			itemRLoc.setVisible(false);
+			itemRLoc.setText(" ");
+			itemCLoc.setVisible(false);
+			itemCLoc.setText(" ");
+			
+			label1.setVisible(false);
+			label2.setVisible(false);
+			label3.setVisible(false);
+			label4.setVisible(false);
+			label5.setVisible(false);
+				
 		});
 	}
+	
 	public static void main(String[] args){
 		Application.launch(args);
 	}
